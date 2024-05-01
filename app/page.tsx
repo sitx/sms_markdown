fc014a0aa18a9a3dd3cc8274cff451697c69de0d
Dmitrii <ska18@mail.ru>
2024-05-01 16:30:47 +0300

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <section className='py-24'>
+      <div className='container'>
+        <div className='text-3xl font-bold'>Next TS Starter</div>
+      </div>
+    </section>
+  )
+}
