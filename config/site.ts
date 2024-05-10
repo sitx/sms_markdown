e1bb3c9e37ddc5fc0943050c8fd9685f25bcbdc1
sitx <ska18@mail.ru>
2024-05-10 21:38:56 +0300

    add content

--- config/site.ts
+++ config/site.ts
@@ -6,7 +6,7 @@ export const siteConfig = {
   url: 'https://sms-sigma-nine.vercel.app',
   description: 'Nextjs 14 blog/sms/other using velite tailwindcss and shadcn',
   author: 'sky_wa1ker',
-  version: '0.20:fix',
+  version: '0.22:content',
   avatar: '/avatar.svg',
   links: {
     site: 'https://sms-sigma-nine.vercel.app/',
